863cd3e68b3df106a27a49f4517a53641f9c044d
cjm322 <conor.murphy@gmail.com>
2020-07-23 22:50:30 -0400

    initial commit

---
+++ collatz_conjecture.py
@@ -0,0 +1,20 @@
+# Python implementation of Collatz conjecture using recursion and memoizatoin
+
+n = int(input('Enter a number: ')) 
+
+cache = dict()
+
+def calc(n):
+  print(n)
+  if n == 1:
+    return n
+  elif n in cache:
+    return n
+  elif n % 2 == 0:
+    a = n/2
+    cache[n] = a
+    return calc(a)
+  else:
+    a = (n * 3) + 1
+    cache[n] = a
+    return calc(a)
